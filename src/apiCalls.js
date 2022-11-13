931bc9d0a9c000d498851840b621f8698d67e206
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-11-13 14:39:27 -0600

    add environmental variable

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,11 @@
-// const apiKey = process.env.REACT_APP_API_KEY
-// const nytURL = process.env.REACT_APP_NYT_URL
+const apiKey = process.env.REACT_APP_API_KEY
+const nytURL = process.env.REACT_APP_NYT_URL
 
 // REACT_APP_NYT_URL = https://api.nytimes.com/svc/topstories/v2/
 // REACT_APP_API_KEY = unC8DJvBitfNbQZ6sygQv7bFd2Wxf9xg
 
-const apiKey = "unC8DJvBitfNbQZ6sygQv7bFd2Wxf9xg"
-const nytURL = "https://api.nytimes.com/svc/topstories/v2/"
+// const apiKey = "unC8DJvBitfNbQZ6sygQv7bFd2Wxf9xg"
+// const nytURL = "https://api.nytimes.com/svc/topstories/v2/"
 // const https://api.nytimes.com/svc/topstories/v2/home.json?api-key=unC8DJvBitfNbQZ6sygQv7bFd2Wxf9xg
 
 const getArticles = async (tag) => {
