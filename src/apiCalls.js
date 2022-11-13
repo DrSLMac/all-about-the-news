f15e886faaa0d6ab49837d92672efe1f3429c7ac
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-11-13 14:22:38 -0600

    updated fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,6 +6,7 @@
 
 const apiKey = "unC8DJvBitfNbQZ6sygQv7bFd2Wxf9xg"
 const nytURL = "https://api.nytimes.com/svc/topstories/v2/"
+// const https://api.nytimes.com/svc/topstories/v2/home.json?api-key=unC8DJvBitfNbQZ6sygQv7bFd2Wxf9xg
 
 const getArticles = async (tag) => {
     try {
